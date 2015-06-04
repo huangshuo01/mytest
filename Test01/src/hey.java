47879a7cdf2c6b69ac866b54f94d296ce40afdfa
Yi1204 <470312571@qq.com>
2015-06-04 11:36:04 +0800

    第三次修改

--- Test01/src/hey.java
+++ Test01/src/hey.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("hahaaaaaaa");
 	System.out.println("hahaaaaaaa");
 	System.out.println("hahaaaaaaa");
+	System.out.println("1234556");
 }
 }
